90555398c05b00b0538c720aa41680ff3506127e
shaulukhov12 <shaulukhov12@gmail.com>
2020-06-29 18:10:25 +0300

    Добавление теста на наличие формы регистрации и логина

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,15 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    REGISTER_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    REGISTER_PASS = (By.CSS_SELECTOR, '#id_registration-password1')
+    REGISTER_PASS_CONF = (By.CSS_SELECTOR, '#id_registration-password2')
+    REGISTER_BUTTON = (By.NAME, '[name="registration_submit"]')
+    LOGIN_EMAIL = (By.CSS_SELECTOR, '#id_login-username')
+    LOGIN_PASS = (By.CSS_SELECTOR, '#id_login-password')
+    LOGIN_BUTTON = (By.NAME, '[name = "login_submit"]')
+    LOGIN_FORM = (By.ID, '#login_form')
+    REGISTER_FORM = (By.ID, '#register_form')
